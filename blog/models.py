78afe5575d20ee793cc162894cbe4e3b1970b209
ujju861994 <ujjwal861994@yahoo.com>
2016-03-23 15:09:25 +0530

    modified template

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
-# Create your models here.
+
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
@@ -15,4 +16,4 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.titless
+        return self.title
